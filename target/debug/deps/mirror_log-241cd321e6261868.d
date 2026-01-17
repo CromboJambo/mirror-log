d643b0165eb9c610eb3e2e48afabb5d3e2652792
crombojambo <dustinjgrover@gmail.com>
2026-01-17 13:37:12 -0600

    forgot to build release before push

--- target/debug/deps/mirror_log-241cd321e6261868.d
+++
@@ -1,9 +0,0 @@
-/home/crombo/mirror-log/target/debug/deps/mirror_log-241cd321e6261868.d: src/main.rs src/db.rs src/log.rs src/view.rs src/schema.sql
-
-/home/crombo/mirror-log/target/debug/deps/libmirror_log-241cd321e6261868.rmeta: src/main.rs src/db.rs src/log.rs src/view.rs src/schema.sql
-
-src/main.rs:
-src/db.rs:
-src/log.rs:
-src/view.rs:
-src/schema.sql:
